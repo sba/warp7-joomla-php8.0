7de09c567ef73b99f7a931ff5609816dde89e1fb
Stefan Bauer <stefan@databauer.ch>
2022-10-26 09:02:56 +0200

    remove wordpress system files

--- warp/systems/wordpress/config/layouts/theme_options.php
+++
@@ -1,8 +0,0 @@
-<div id="warp" class="wrap" data-warp="theme">
-
-    <form id="theme-options" method="post" action="">
-
-        <?php echo $this->render('config:layouts/config') ?>
-    </form>
-
-</div>
--- warp/systems/wordpress/layouts/widgets/rss.php
+++
@@ -1,5 +0,0 @@
-<?php echo $widget->content; ?>
-
-<p>
-    <a class="uk-button" href="<?php echo $widget->params['url']; ?>" target="_blank"><?php _e('Read feed'); ?></a>
-</p>
